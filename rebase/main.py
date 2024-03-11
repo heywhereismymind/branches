960cace05d2984fdff645d375aee9493603d0d38
zabor <zaborka18@gmail.com>
2024-03-11 21:51:18 +0300

    fix from fix branch

---
+++ rebase/main.py
@@ -0,0 +1,18 @@
+def lazy_function(a, b):
+    print('blablabla')
+
+    print(a, b)
+    print('hello boys')
+
+
+def new():
+    pass
+
+
+def my_future_function():
+    """"makes some things"""
+
+    a = 44
+    b = 19
+
+    return a+b
